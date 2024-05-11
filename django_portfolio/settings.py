3f601dd024a46a25643993489676ecc9c05f4c39
JelexS <149525719+JelexS@users.noreply.github.com>
2024-05-10 20:22:38 -0500

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 #DEBUG = True
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = [hola]
-hola='https://django-87-u6yf.onrender.com'
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
