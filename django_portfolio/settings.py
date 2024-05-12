0c1493863e41d290fc066da74308eba89546d2ff
JelexS <149525719+JelexS@users.noreply.github.com>
2024-05-12 12:03:48 -0500

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = 'RENDER' in os.environ
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
